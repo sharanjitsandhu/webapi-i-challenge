230a578e2152a7501eaae67e704ec401b7b2868b
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-04-22 13:19:14 -0700

    Server is running on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+const express = require("express"); //commonJS module import
+
+const server = express();
+
+server.listen(5000, () => {
+  console.log("\n*** API is running on port 5000 ***\n");
+});
